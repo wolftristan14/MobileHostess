e7edfbcee562ab981130d0e937baffe1b1e4114e
Tristan Wolf <wolftristan@hotmail.com>
2017-03-22 17:29:03 -0400

    Added AddRestaurantViewController with basic storyboard layout

---
+++ MobileHostess/AddRestaurantViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddRestaurantViewController.swift
+//  MobileHostess
+//
+//  Created by Tristan Wolf on 2017-03-22.
+//  Copyright © 2017 Tristan Wolf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddRestaurantViewController:UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
