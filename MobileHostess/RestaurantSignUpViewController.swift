8e18220c63d18438bbdf92e55d707213459e1bc3
Tristan Wolf <wolftristan@hotmail.com>
2017-03-22 17:55:16 -0400

    Added two new view controllers for signing up as a restaurant and logging in as a restaurant.  Added buttons for navigating between the different login/signup screens but they aren't implemented yet.

---
+++ MobileHostess/RestaurantSignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantSignUpViewController.swift
+//  MobileHostess
+//
+//  Created by Tristan Wolf on 2017-03-22.
+//  Copyright © 2017 Tristan Wolf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RestaurantSignUpViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
