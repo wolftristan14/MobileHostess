d19e0e2990f21b3fcae25ee2e4037f48f0beae73
Tristan Wolf <wolftristan@hotmail.com>
2017-03-23 12:00:05 -0400

    Changed AddRestaurantViewController so restaurants can change wait times based on party size in whatever way they are used to

---
+++ MobileHostess/EditRestaurantViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EditRestaurantViewController.swift
+//  MobileHostess
+//
+//  Created by Tristan Wolf on 2017-03-22.
+//  Copyright © 2017 Tristan Wolf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditRestaurantViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
