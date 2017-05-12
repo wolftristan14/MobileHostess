16e7e5a5c492dac29e1c9d62638bc1a9e42aa1a9
Tristan Wolf <wolftristan@hotmail.com>
2017-05-12 16:54:20 -0400

    Restaurant Details can now be edited (right now only works if EditRestaurantViewController is pushed on top of AddRestaurantViewController i.e. when you sign up a new restaurant)

--- MobileHostess/EditRestaurantViewController.swift
+++ MobileHostess/EditRestaurantViewController.swift
@@ -58,8 +58,7 @@ class EditRestaurantViewController:UIViewController, UIPickerViewDelegate, UIPic
             }
             
             self.updatePickerView()
-
-        
+            //
         })
         
         
